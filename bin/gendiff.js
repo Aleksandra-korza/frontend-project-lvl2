29e67f3d065f2f1737223ce7df6fb056e44be8fc
Aleksandra-korza <Aleksandra.korza@mail.ru>
2022-01-05 18:16:54 +0100

    index.js 2 step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,6 +8,6 @@ program
     .version("0.1.0")
     .description("Compares two configuration files and shows a difference.")
     .helpOption("-h, --help", "output usage information")
-    .option("-f, --format", <type>,  "output format")
-    .arguments()
+    .option("-f, --format [type]",  "output format")
+    .arguments('<filepath1> <filepath2>')
     .parse(process.argv);

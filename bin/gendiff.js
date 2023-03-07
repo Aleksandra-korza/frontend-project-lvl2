a4ba0cee1c21979474996736c002b7678855ac1b
Aleksandra-korza <Aleksandra.korza@mail.ru>
2023-03-07 18:27:34 +0100

    переведено с строку2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -13,11 +13,12 @@ program
     .helpOption("-h, --help", "output usage information")
     .option("-f, --format [type]",  "output format")
     .action((filepath1, filepath2) => {
-        const path1 = path.resolve(process.cwd(), filepath1); // конструируем полный путь эprocess.cwd()э  -  созд абсолютный путь. путь от корневой директории. соединяет 2 пути : 1 пользователя до  файла, 2й - от корня компа до папки из которой вызываем програму
+        const path1 = path.resolve(process.cwd(), filepath1); // конструируем полный путь process.cwd()  -  созд абсолютный путь. путь от корневой директории. соединяет 2 пути : 1 пользователя до  файла, 2й - от корня компа до папки из которой вызываем програму
         const path2 = path.resolve(process.cwd(), filepath2);
+
         const file1 = fs.readFileSync(path1,'utf-8'); // fs - модуль для работы с файловой системой на JS, там много разных функций, 'utf-8' - кодировка файла. самая популярная кодировка. что бы получить текс, нужно добавить эту кодировку
         const file2 = fs.readFileSync(path2,'utf-8');
-        const obj1 = JSON.parse(file1);
+        const obj1 = JSON.parse(file1); // распарсили файлы JSON.parse(file1): из JSON строки перевели в вид объекта. 
         const obj2 = JSON.parse(file2);
         console.log(gendiff(obj1,obj2));
     })
--- index.js
+++ index.js
@@ -2,47 +2,64 @@ import lodash from 'lodash';
 
 const gendiff = (obj1, obj2) => {
     const keys = lodash.sortBy(lodash.union(lodash.keys(obj1), lodash.keys(obj2))); // набор уникальных ключей двух файлов
-    return keys.map((key) => {  // перебираем уникальные ключи по очереди
+    const keyFiltr = keys.map((key) => { // John // перебираем уникальные ключи по очереди
         if (!lodash.has(obj1, key)) {  // если в первом файле нет ключа, то
             return {  // возвращаем ключ для второго файла и типируем как 'added' 
+                type: '+',
                 key: key,
-                type: 'added',
                 value: obj2[key]
             } // если 
         }
         if (!lodash.has(obj2, key)) {  //  если во втором файле нет ключа , то 
             return {  // записываем ключь первого файла и типируем как 'removed'
+                type: '-',
                 key: key,
-                type: 'removed',
                 value: obj1[key]
             }
         }
         if ((lodash.has(obj1, key)) || (lodash.has(obj2, key))) { // если  ключи совпадают
             if (obj1[key] !== obj2[key]) {
-                return [  // возвращаем ключ первого файла и второго файла
+                return [ // возвращаем ключ первого файла и второго файла
                     {
+                        type: '-',
                         key: key,
-                        type: 'changed',
                         value: obj1[key]
                     },
                     {
+                        type: '+',
                         key: key,
-                        type: 'changed',
                         value: obj2[key]
                     }
                 ]
             }
 
             return {  // возвращаем ключ первого файла
+                type: ' ',
                 key: key,
-                type: 'unchanged',
                 value: obj1[key]
             }
 
         }
 
-    });
+    })
+   
+    const step3filtr = keyFiltr.map((arr) => {
+
+if (Array.isArray(arr)) {
+     return arr.map((ar) => {
+         
+         return Object.values(`${ar.type} ${ar.key}: ${ar.value}`).join('');
+    })
 }
+        return Object.values(`${arr.type} ${arr.key}: ${arr.value}`).join('');
+   
+    }
+               
+);
+
+    return JSON.stringify(step3filtr.flat(), null, '\t');
+};
+
 
 
 export default gendiff;

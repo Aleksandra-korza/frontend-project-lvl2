693fdda4878a5f5c4bc0b87929b1025fea5b38fa
Aleksandra-korza <Aleksandra.korza@mail.ru>
2023-09-19 19:31:07 +0200

    шаг 3

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const gendiff = (obj1, obj2) => {
     const keyFiltr = keys.map((key) => { // John // перебираем уникальные ключи по очереди
         if (!lodash.has(obj1, key)) {  // если в первом файле нет ключа, то
             return {  // возвращаем ключ для второго файла и типируем как 'added' 
-                type: '+',
+                type: '+',//added'
                 key: key,
                 value: obj2[key]
             } // если 
@@ -21,7 +21,7 @@ const gendiff = (obj1, obj2) => {
             if (obj1[key] !== obj2[key]) {
                 return [ // возвращаем ключ первого файла и второго файла
                     {
-                        type: '-',
+                        type: '-',//updated
                         key: key,
                         value: obj1[key]
                     },
@@ -46,19 +46,24 @@ const gendiff = (obj1, obj2) => {
     const step3filtr = keyFiltr.map((arr) => {
 
 if (Array.isArray(arr)) {
-     return arr.map((ar) => {
-         
-         return Object.values(`${ar.type} ${ar.key}: ${ar.value}`).join('');
-    })
+     //return arr.map((ar) => {
+         const d = arr.map((ar) => {
+         return step3filtr(ar);
+         //Object.values(`${ar.type} ${ar.key}: ${ar.value}`).join('');
+    }) 
+    return d;
 }
+
         return Object.values(`${arr.type} ${arr.key}: ${arr.value}`).join('');
    
     }
                
 );
+const result = step3filtr.flat();
 
-    return JSON.stringify(step3filtr.flat(), null, '\t');
+    return JSON.stringify(result, ' ', ' ');
 };
+
 
 
 

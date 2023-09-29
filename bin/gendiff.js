7c2028fc2605b8eef2cf485943a14c49b813a3cb
Aleksandra-korza <Aleksandra.korza@mail.ru>
2023-09-29 11:50:14 +0200

    stylish

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,26 +1,30 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander'; // исполняемый файл это и он должен быть в папке bin.
-import gendiff from '../index.js';
 import fs from 'fs';
 import path from 'path';
+import gendiff from '../index.js';
 
-const program = new Command(); // взяли из библиотеки класс команд, создали экземпляр этого класса и будем с помощью методов делать выводы на экран версии и хелп и тд. 
+const program = new Command(); // взяли из библиотеки класс команд,
+// создали экземпляр этого класса и будем с помощью методов делать выводы на экран версии и хелп и тд.
 
 program
-    .version("0.1.0")
-    .description("Compares two configuration files and shows a difference.")
-    .helpOption("-h, --help", "output usage information")
-    .option("-f, --format [type]",  "output format")
-    .action((filepath1, filepath2) => {
-        const path1 = path.resolve(process.cwd(), filepath1); // конструируем полный путь process.cwd()  -  созд абсолютный путь. путь от корневой директории. соединяет 2 пути : 1 пользователя до  файла, 2й - от корня компа до папки из которой вызываем програму
-        const path2 = path.resolve(process.cwd(), filepath2);
+  .version('0.1.0')
+  .description('Compares two configuration files and shows a difference.')
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format [type]', 'output format')
+  .action((filepath1, filepath2) => {
+    const path1 = path.resolve(process.cwd(), filepath1); // конструируем полный путь process.cwd()
+    // -  созд абсолютный путь. путь от корневой директории. соединяет 2 пути : 1 пользователя до  файла,
+    // 2й - от корня компа до папки из которой вызываем програму
+    const path2 = path.resolve(process.cwd(), filepath2);
 
-        const file1 = fs.readFileSync(path1,'utf-8'); // fs - модуль для работы с файловой системой на JS, там много разных функций, 'utf-8' - кодировка файла. самая популярная кодировка. что бы получить текс, нужно добавить эту кодировку
-        const file2 = fs.readFileSync(path2,'utf-8');
-        const obj1 = JSON.parse(file1); // распарсили файлы JSON.parse(file1): из JSON строки перевели в вид объекта. 
-        const obj2 = JSON.parse(file2);
-        console.log(gendiff(obj1,obj2));
-    })
-    .arguments('<filepath1> <filepath2>')
-    .parse(process.argv);
+    const file1 = fs.readFileSync(path1, 'utf-8'); // fs - модуль для работы с файловой системой на JS, т
+    // ам много разных функций, 'utf-8' - кодировка файла. самая популярная кодировка. что бы получить текс, нужно добавить эту кодировку
+    const file2 = fs.readFileSync(path2, 'utf-8');
+    const obj1 = JSON.parse(file1); // распарсили файлы JSON.parse(file1): из JSON строки перевели в вид объекта.
+    const obj2 = JSON.parse(file2);
+    console.log(gendiff(obj1, obj2));
+  })
+  .arguments('<filepath1> <filepath2>')
+  .parse(process.argv);
--- index.js
+++ index.js
@@ -1,117 +1,96 @@
 import lodash from 'lodash';
 import _ from 'lodash';
 
-
 function gendiff(obj1, obj2) {
+  const keys = lodash.sortBy(lodash.union(lodash.keys(obj1), lodash.keys(obj2))); // набор уникальных ключей двух файлов
 
+  const tree = {};
 
-    const keys = lodash.sortBy(lodash.union(lodash.keys(obj1), lodash.keys(obj2))); // набор уникальных ключей двух файлов
+  const keyFiltr = keys.reduce((tree, key) => {
+    if (!lodash.has(obj1, key)) { // если в первом файле нет ключа, то
+      if (_.isObject(obj2[key])) {
+        gendiff(obj2[key]);
+      }
+      return tree = {
+        type: 'added',
+        // sign: '+',
+        key,
+        value: obj2[key],
+      };
+    }
 
-let tree = {};
+    if (!lodash.has(obj2, key)) { //  если во втором файле нет ключа , то
+      if (_.isObject(obj1[key])) {
+        gendiff(obj1[key]);
+      }
 
-    let keyFiltr = keys.reduce((tree, key) => {
+      return tree = {
+        type: 'removed',
+        // sign: '-',
+        key,
+        value: obj1[key],
+      };
+    }
+    if ((lodash.has(obj1, key)) || (lodash.has(obj2, key))) { // если  ключи совпадают
+      if ((obj1[key] !== obj2[key]) && (!_.isObject(obj2[key]))) {
+        return [
+          {
+            type: 'changed',
+            key,
+            // sign: '-',
+            value: obj1[key],
 
-        if (!lodash.has(obj1, key)) { // если в первом файле нет ключа, то
+            // sign: '+',
+            value2: obj2[key],
+            // children: []
+          },
+        ];
+      }
+      if ((obj1[key] !== obj2[key]) && (_.isObject(obj1[key]) || (_.isObject(obj2[key])))) {
+        gendiff(obj1[key], obj2[key]);
+        // {
+        //     key: key,
+        //     type: 'changed',
+        //     value: gendiff(obj1[key], obj2[key])
+        // };
+      }
+      return {
+        type: 'unchanged',
+        // sign: ' ',
+        key,
+        value: obj1[key],
+      };
+    }
+  }, {});
 
-            if (_.isObject(obj2[key])) {
+  const stringify = (value, replacer = ' ', spacesCount = 1) => {
+    const iter = (carentValue, deph) => {
+      if (typeof carentValue !== 'object' || carentValue === null) {
+        return String(carentValue);
+      }
 
-            gendiff(obj2[key]);
-            }
-            return tree = {
-                type: 'added',
-                //sign: '+',
-                key: key,
-                value: obj2[key]
-            }
-            
-        }
-        
-        if (!lodash.has(obj2, key)) {  //  если во втором файле нет ключа , то 
+      const indenSize = spacesCount * deph;
+      const currentIndent = replacer.repeat(indenSize);
+      const bracetIndent = replacer.repeat(indenSize - spacesCount);
 
-            if (_.isObject(obj1[key])) {
+      const array = Object.entries(carentValue);
 
-                gendiff(obj1[key]);
-             }
+      const str = array.map(([key, val]) => `${currentIndent}${key}: ${iter(val, deph + 4)}`);
 
-            return tree =  
-                {type: 'removed',
-                //sign: '-',
-                key: key,
-                value: obj1[key]}
-            
-        }
-        if ((lodash.has(obj1, key)) || (lodash.has(obj2, key))) { // если  ключи совпадают
-            if ((obj1[key] !== obj2[key]) && (!_.isObject(obj2[key]))) {
-                 return [
-                    {
-                        type: 'changed',
-                        key: key,
-                        //sign: '-',
-                        value: obj1[key],
+      const result = ['{', ...str, `${bracetIndent}}`].join('\n');
 
-                        //sign: '+',
-                        value2: obj2[key],
-                        //children: []
-                    }
-                ];
-            }
-            else if ((obj1[key] !== obj2[key]) && (_.isObject(obj1[key]) || (_.isObject(obj2[key])))) {
-                 gendiff(obj1[key], obj2[key]);
-                 //{
-                //     key: key,
-                //     type: 'changed',
-                //     value: gendiff(obj1[key], obj2[key])
-                // };
-            }
-            return {
-                type: 'unchanged',
-                //sign: ' ',
-                key: key,
-                value: obj1[key]
-            };
-
-        }
-
-        
-    }, {});
-
-
-    const stringify = (value, replacer = ' ', spacesCount = 1) => {
-
-        const iter = (carentValue, deph) => {
-
-            if (typeof carentValue !== 'object' || carentValue === null) {
-
-                return String(carentValue);
-
-            }
-
-            const indenSize = spacesCount * deph;
-            const currentIndent = replacer.repeat(indenSize);
-            const bracetIndent = replacer.repeat(indenSize - spacesCount);
-
-            const array = Object.entries(carentValue);
-
-            const str = array.map(([key, val]) => `${currentIndent}${key}: ${iter(val, deph + 4)}`);
-
-            const result = ['{', ...str, `${bracetIndent}}`].join('\n');
-
-            return result;
-
-        };
-
-        return iter(value, 1);
+      return result;
     };
 
+    return iter(value, 1);
+  };
 
-    //return 
-   // keyFiltr.flat();
+  // return
+  // keyFiltr.flat();
 
-    return keyFiltr;
+  return keyFiltr;
 
-
-    //return stringify(keyFiltr.flat());
-
-};
+  // return stringify(keyFiltr.flat());
+}
 
 export default gendiff;

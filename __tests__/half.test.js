885515eb0f389b912267c80664fccefef0ce73c5
Aleksandra-korza <Aleksandra.korza@mail.ru>
2023-10-18 16:31:46 +0200

    README.md

--- __tests__/half.test.js
+++ __tests__/half.test.js
@@ -9,18 +9,28 @@ const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '_fixtures_', filename);
 
-test('gendiff', () => {
-  expect(gendiff('file1.json', 'file2.json')).toBe(fs.readFileSync(getFixturePath('resultStylish.txt'), 'utf-8'));
+test.each([
+  {file1: 'file1.json', file2: 'file2.json', formatter: '', expected: 'resultStylish.txt'},
+  {file1: 'file1.json', file2: 'file2.json', formatter: 'stylish', expected: 'resultStylish.txt'},
+  {file1: 'file1.json', file2: 'file2.json', formatter: 'plain', expected: 'resultPlain.txt'},
+  {file1: 'file1.json', file2: 'file2.json', formatter: 'json', expected: 'resultJson.txt'},
+])('gendiff', ({file1, file2, formatter, expected}) => {
+  expect(gendiff(file1, file2, formatter)).toBe(fs.readFileSync(getFixturePath(expected), 'utf-8'));
 });
 
-test('gendiff', () => {
-  expect(gendiff('file1.json', 'file2.json', 'stylish')).toBe(fs.readFileSync(getFixturePath('resultStylish.txt'), 'utf-8'));
-});
 
-test('gendiff-plain', () => {
-  expect(gendiff('file1.json', 'file2.json', 'plain')).toBe(fs.readFileSync(getFixturePath('resultPlain.txt'), 'utf-8'));
-});
+// test('gendiff', () => {
+//   expect(gendiff('file1.json', 'file2.json')).toBe(fs.readFileSync(getFixturePath('resultStylish.txt'), 'utf-8'));
+// });
 
-test('gendiff-json', () => {
-  expect(gendiff('file1.json', 'file2.json', 'json')).toBe(fs.readFileSync(getFixturePath('resultJson.txt'), 'utf-8'));
-});
+// // test('gendiff-stylish', () => {
+// //   expect(gendiff('file1.json', 'file2.json', 'stylish')).toBe(fs.readFileSync(getFixturePath('resultStylish.txt'), 'utf-8'));
+// // });
+
+// test('gendiff-plain', () => {
+//   expect(gendiff('file1.json', 'file2.json', 'plain')).toBe(fs.readFileSync(getFixturePath('resultPlain.txt'), 'utf-8'));
+// });
+
+// test('gendiff-json', () => {
+//   expect(gendiff('file1.json', 'file2.json', 'json')).toBe(fs.readFileSync(getFixturePath('resultJson.txt'), 'utf-8'));
+// });

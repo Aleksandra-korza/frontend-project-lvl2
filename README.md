f53f88393636b67b10b960f409f0f4860efdfa6f
Aleksandra-korza <Aleksandra.korza@mail.ru>
2023-10-18 19:36:36 +0200

    README.md

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 ### Hexlet tests and linter status:
-[![Actions Status](https://github.com/Aleksandra-korza/frontend-project-lvl2/workflows/hexlet-check/badge.svg)](https://github.com/Aleksandra-korza/)frontend-project-lvl2/actions
+
+[![Actions Status](https://github.com/Aleksandra-korza/frontend-project-lvl2/workflows/hexlet-check/badge.svg)](https://github.com/Aleksandra-korza/)
 
 [![Build Status](https://github.com/Aleksandra-korza/frontend-project-lvl2/workflows/hexlet-check/badge.svg)](https://github.com/Aleksandra-korza/)
 
--- __tests__/half.test.js
+++ __tests__/half.test.js
@@ -7,30 +7,23 @@ import gendiff from '../index.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const getFixturePath = (filename) => path.join(__dirname, '..', '_fixtures_', filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 test.each([
-  {file1: 'file1.json', file2: 'file2.json', formatter: '', expected: 'resultStylish.txt'},
-  {file1: 'file1.json', file2: 'file2.json', formatter: 'stylish', expected: 'resultStylish.txt'},
-  {file1: 'file1.json', file2: 'file2.json', formatter: 'plain', expected: 'resultPlain.txt'},
-  {file1: 'file1.json', file2: 'file2.json', formatter: 'json', expected: 'resultJson.txt'},
-])('gendiff', ({file1, file2, formatter, expected}) => {
+  {
+    file1: 'file1.json', file2: 'file2.json', formatter: '', expected: 'resultStylish.txt',
+  },
+  {
+    file1: 'file1.json', file2: 'file2.json', formatter: 'stylish', expected: 'resultStylish.txt',
+  },
+  {
+    file1: 'file1.json', file2: 'file2.json', formatter: 'plain', expected: 'resultPlain.txt',
+  },
+  {
+    file1: 'file1.json', file2: 'file2.json', formatter: 'json', expected: 'resultJson.txt',
+  },
+])('gendiff', ({
+  file1, file2, formatter, expected,
+}) => {
   expect(gendiff(file1, file2, formatter)).toBe(fs.readFileSync(getFixturePath(expected), 'utf-8'));
 });
-
-
-// test('gendiff', () => {
-//   expect(gendiff('file1.json', 'file2.json')).toBe(fs.readFileSync(getFixturePath('resultStylish.txt'), 'utf-8'));
-// });
-
-// // test('gendiff-stylish', () => {
-// //   expect(gendiff('file1.json', 'file2.json', 'stylish')).toBe(fs.readFileSync(getFixturePath('resultStylish.txt'), 'utf-8'));
-// // });
-
-// test('gendiff-plain', () => {
-//   expect(gendiff('file1.json', 'file2.json', 'plain')).toBe(fs.readFileSync(getFixturePath('resultPlain.txt'), 'utf-8'));
-// });
-
-// test('gendiff-json', () => {
-//   expect(gendiff('file1.json', 'file2.json', 'json')).toBe(fs.readFileSync(getFixturePath('resultJson.txt'), 'utf-8'));
-// });

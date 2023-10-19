4cdfd75b229fd4ac995af80c389c91588ca3afbd
Aleksandra-korza <Aleksandra.korza@mail.ru>
2023-10-19 19:02:59 +0200

    test

--- scr/index.js
+++ scr/index.js
@@ -4,15 +4,13 @@ import takeData from './formatters/parses.js';
 import buildNewObj from './formatters/buildNewObj.js';
 
 function gendiff(file1, file2, format) {
-  // if ((path.extname(file1) === '.json' || path.extname(file1) === '.yml' || path.extname(file1) === '.yaml')
-  // && (path.extname(file2) === '.json' || path.extname(file2) === '.yml' || path.extname(file2) === '.yaml')) {
-    const object1 = takeData(file1);
-    const object2 = takeData(file2);
+  const object1 = takeData(file1);
+  const object2 = takeData(file2);
 
-    const tree = buildNewObj(object1, object2);
+  const tree = buildNewObj(object1, object2);
 
-    return selectFormat(tree, format);
-  }
+  return selectFormat(tree, format);
+}
 
 //   return 'The program does not work with this format';
 // }

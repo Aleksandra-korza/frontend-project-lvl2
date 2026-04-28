a11212f2d300de13a9d532df30a7ce59fc41a973
Aleksandra-korza <75067469+Aleksandra-korza@users.noreply.github.com>
2026-04-28 12:03:55 +0200

    Update half.test.js

--- __tests__/half.test.js
+++ __tests__/half.test.js
@@ -5,7 +5,7 @@ import fs from 'fs'
 import gendiff from '../scr/index.js'
 
 const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename);
+const __dirname = dirname(__filename)
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename)
 

afd5fc63194ccae042a6b205e2c7d9d8d9f2a41b
Aleksandra <aleksandra.korza@mail.ru>
2026-05-13 11:18:18 +0200

    swich

--- src/formatters/selectFileExtension.js
+++ src/formatters/selectFileExtension.js
@@ -4,16 +4,16 @@ import path from 'path'
 const selectFileExtension = (filepath, file) => {
   switch (path.extname(filepath)) {
     case '.json':
-    return JSON.parse(file) // расп. файлы JSON.parse(file1):изJSON строки->в вид обj
+      return JSON.parse(file) // расп. файлы JSON.parse(file1):изJSON строки->в вид обj
 
     case '.yml':
-    case'.yaml':
-    return yaml.load(file) // расп.файлы yaml.load(file):из yamlстроки->в видобj
+    case '.yaml':
+      return yaml.load(file) // расп.файлы yaml.load(file):из yamlстроки->в видобj
 
     default:
-          console.error(`Unknown file extension.`)
-          return 'Unknown file extension.'
-}
+      console.error('Unknown file extension.')
+      return 'Unknown file extension.'
+  }
 }
 
 export default selectFileExtension
